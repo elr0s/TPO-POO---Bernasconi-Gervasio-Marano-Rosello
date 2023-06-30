24596947fdf8b157aae4563ea55b264d78a7c31c
sebaBernasconi <bernaseba1@gmail.com>
2023-06-30 11:41:49 -0300

    metodos del cliente listo

--- src/obj/ManoDeObra.java
+++ src/obj/ManoDeObra.java
@@ -1,17 +1,25 @@
 package obj;
 
 public class ManoDeObra {
-    /*
+
     private int codigo;
     private String descripcion;
-    private float precio;
     private Tecnico tecnico;
     private float valorHora;
 
     public boolean soyEsaManoDeObra(int codigo){
-
-
-
+        return this.codigo == codigo;
     }
-    */
+
+    public void registrarManoDeObra(int codigo, String desc, Tecnico t, float valH){
+        this.codigo = codigo;
+        this.descripcion = desc;
+        this.tecnico = t;
+        this.valorHora = valH;
+    }
+
+    public float obtenerValH(){
+        return valorHora;
+    }
+
 }
