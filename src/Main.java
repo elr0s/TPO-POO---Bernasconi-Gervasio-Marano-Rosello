f97f283715c9165b09fa0584efcc157d60754c95
MatiasMarano10 <115367312+MatiasMarano10@users.noreply.github.com>
2023-06-30 09:56:38 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+package ventana;
+
+import javax.swing.SwingUtilities;
+
+public class main {
+
+	public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new ventana();
+            }
+        });
     }
+	
 }
